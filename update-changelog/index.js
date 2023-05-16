b858448b5692b2ccb7c03e82e0ca8203d099dc31
George Shestayev <george@uniques.ca>
2023-05-16 08:04:12 -0700

    Update: improved `update-changelog` action to allow changelog retrieval between any two tags Fix: fixed `publish-github-release` to properly retrieve changelog

--- update-changelog/index.js
+++ update-changelog/index.js
@@ -1,22 +1,37 @@
 import core from "@actions/core";
 import fs from "fs";
-import { updateChangelog, getChangelog } from "../src/changelog.js";
+import { updateChangelog, generateChangelog } from "../src/changelog.js";
+import { getLastTag, getTag } from "../src/git.js";
 
 const changelogPath = core.getInput('changelog_path');
 const targetVersion = core.getInput('target_version');
-const raw = core.getInput('raw');
+const mode = core.getInput('mode');
+const offset = core.getInput('offset');
 if (!changelogPath || !targetVersion) {
-    console.error('Usage: node changelog.js CHANGELOG_PATH TARGET_VERSION RAW');
+    console.error('Usage: node changelog.js CHANGELOG_PATH TARGET_VERSION ["normal"|"raw"] [OFFSET]');
     process.exit(1);
 }
 
-console.log(raw);
-if (raw == 'true' || raw == '1') {
+if (!mode) {
+    mode = 'normal';
+}
+
+let startTag, endTag;
+if (!offset) {
+    offset = 0;
+    startTag = getLastTag();
+    endTag = 'HEAD';
+}
+else {
+    startTag = getTag(Number(offset) + 1);
+    endTag = getTag(offset)
+}
+
+if (mode == 'raw') {
     console.log('Generating raw changelog...')
-    const changelog = getChangelog();
-    console.log(changelog)
+    const changelog = generateChangelog(startTag, endTag);
     fs.writeFileSync(changelogPath, changelog);
 } else {
     console.log('Updating changelog in normal (non-raw) mode...')
-    updateChangelog(changelogPath, targetVersion);
+    updateChangelog(changelogPath, targetVersion, startTag, endTag);
 }
