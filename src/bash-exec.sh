fc45fc0e7bab4f4827c702c9ea4f04de25e641cc
Ihor Ziubrovskyi <ihor@uniques.ca>
2023-05-31 04:59:14 +0300

    Update: publish-github-release

--- src/bash-exec.sh
+++ src/bash-exec.sh
@@ -3,16 +3,52 @@
 
 CHECK_PASSED=true
 
-FOLDERS=$2
-
-# TODO We need to check if a dot is specified and set SEARCH_IN_ROOT to true if yes
-#   and remove the comma properly then
-SEARCH_IN_ROOT=false
+FOLDERS="${{ inputs.exec_bit_check_paths }}"
+if [[ "$FOLDERS" == .* ]]; then
+  SEARCH_IN_ROOT=true
+fi
 
 # Remove spaces
 FOLDERS=${FOLDERS// /}
+
+# Adding comma at the beginning and the end
+FOLDERS=",$FOLDERS,"
+
+# Remove double commas
+FOLDERS=${FOLDERS//,,/,}
+
+# Search in root if ,., is specified
+if [[ "$FOLDERS" == *,.,* ]]; then
+  SEARCH_IN_ROOT=true
+  # Replace ,., with comma
+  FOLDERS=${FOLDERS//,.,/,}
+else
+  SEARCH_IN_ROOT=false
+fi
+
+# Replace slashes with escaped slashes
+FOLDERS=${FOLDERS//\//\\/}
+
+# Remove the first and the last comma
+FOLDERS=${FOLDERS:1:-1}
+
+EXTENSIONS="${{ inputs.exec_bit_check_extensions }}"
+
+# Adding comma at the beginning and the end
+EXTENSIONS=",$EXTENSIONS,"
+
+# Remove double commas
+EXTENSIONS=${EXTENSIONS//,,/,}
+
+# Replace slashes with escaped slashes
+EXTENSIONS=${EXTENSIONS//\//\\/}
+
+# Remove the first and the last comma
+EXTENSIONS=${EXTENSIONS:1:-1}
+
 # Replace comma with slash and vertical separator
 FOLDERS=${FOLDERS//,/\/\|}
+
 if [ -z "$FOLDERS" ]; then
   # No folders specified
   FOLDERS=".*"
@@ -21,11 +57,6 @@ else
   FOLDERS="$FOLDERS\/"
 fi
 
-EXTENSIONS=$3
-# Remove spaces
-EXTENSIONS=${EXTENSIONS// /}
-# Replace comma with vertical separator
-EXTENSIONS=${EXTENSIONS//,/\|}
 if [ -z "$EXTENSIONS" ]; then
   # No extensions specified, looking for all
   EXTENSIONS=".*"
@@ -33,18 +64,19 @@ fi
 
 REGEX="\.\/($FOLDERS).*\.($EXTENSIONS)"
 
-if [ $SEARCH_IN_ROOT ]; then
-  REGEX="$REGEX|\.\/[^\/]*(sh)"
+if [ $SEARCH_IN_ROOT == true ]; then
+  REGEX="$REGEX|\.\/[^\/]*($EXTENSIONS)"
 fi
 
 echo "$REGEX"
 
-find $1 -type f -regextype posix-extended -regex "REGEX" | while read file; do
+find . -type f -regextype posix-extended -regex "$REGEX" | while read file; do
   if [ ! -x "$file" ]; then
     echo "$file is not executable"
     CHECK_PASSED=false
   fi
 done
-if $CHECK_PASSED; then
+
+if $CHECK_PASSED ; then
   exit 1
 fi
