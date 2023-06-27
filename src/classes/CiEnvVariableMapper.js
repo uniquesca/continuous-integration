8cf3b0cb53217fc5e5e71d47b44e89eb0156ec08
George Shestayev <georges@uniques.ca>
2023-06-27 06:31:48 -0700

    Debug: added additional debug output

--- src/classes/CiEnvVariableMapper.js
+++ src/classes/CiEnvVariableMapper.js
@@ -12,10 +12,13 @@ export class CiEnvVariableMapper {
     map() {
         const result = {};
         for (const key in this.variables) {
+            core.info("Processing variable: " + key);
             if (this.environment.token_mappings.hasOwnProperty(key)) {
                 result[this.environment.token_mappings[key]] = this.variables[key];
+                core.info("Mapping found: " + this.environment.token_mappings[key]);
             } else {
                 result[key] = this.variables[key];
+                core.info("Mapping not found.");
             }
         }
         return result;
